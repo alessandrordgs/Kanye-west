87781772f7e9e3cb50101bca0152706daf337ea8
forge1010 <ale.silva1120@gmail.com>
2021-09-07 17:23:33 -0300

    refactor: deixando o codigo mais legivel

--- src/App.jsx
+++ src/App.jsx
@@ -11,60 +11,62 @@ import "./styles/main.css"
 function App() {
 
   const [kanyeButton, setKanyeButton] = useState(true)
-  const [kanye, setKanye] = useState("");
-  const [music, setMusic] = useState("")
+  const [kanyeQuote, setKanyeQuote] = useState("");
+  const [musica, setMusica] = useState("")
   const handleKanye = () => setKanyeButton(!kanyeButton)
   
   function newMusic(){
-    const randomMusicIndex = Math.floor(Math.random() * Musicas.length);
-    const musica = Musicas[randomMusicIndex];
+    const randomMusicaIndex = Math.floor(Math.random() * Musicas.length);
+    const musica = Musicas[randomMusicaIndex];
 
-    setMusic(musica)
+    setMusica(musica)
   }
-
-  console.log(music)
-
-  const Use = kanyeButton === false
 
   useEffect(()=> 
   {api.get("").then(response => {
-    setKanye(response.data)
-
+    const {quote} = response.data
+    setKanyeQuote(quote) 
   })
-},[Use])
+},[kanyeButton])
    
   return (
-    <>
+ 
      <div className="App" >
-       
-       <header>
-       <h1>Kanye West</h1>
-       <p>Aperte no botão e veja frases icônicas do Ye</p>
-       </header> 
+        
+      <header>
+        <h1>Kanye West</h1>
+        <p>Aperte no botão e veja frases icônicas do Ye</p>
+      </header> 
 
-     <div>
-      <img src={Kanye} alt="" />
-     </div>
+      <div>
+        <img src={Kanye} alt="Kanye West" />
+      </div>
+        
+      <div className="quote margin">
+        {kanyeButton ?
+        <p>{kanyeQuote}</p> : 
+        <p>{kanyeQuote}</p>}
+        <p className="kanye"> Kanye West</p>
+      </div>
       
-     <div className="quote margin">
-       {kanyeButton ? <p>{kanye.quote}</p> :<p>{kanye.quote}</p>}
-       <p className="kanye"> Kanye West</p>
-     </div>
-     <div className="quote">
-        <p> <a href={`https://www.youtube.com/results?search_query=Kanye+West+${music.music}`} target="_blank" rel="noopener noreferrer">{music.music}</a></p> 
-     </div>
+      <div className="quote">
+        <p> 
+          <a href={`https://www.youtube.com/results?search_query=Kanye+West+${musica.music}`} target="_blank" rel="noopener noreferrer">
+            {musica.music}
+          </a>
+        </p> 
+      </div>
 
-      <button onClick={handleKanye}>Fale comigo Kanye</button>
-  
-      <button onClick={newMusic}>Peça uma musica para Kanye</button>
-      
-      <footer> <a href="https://alessandrordgs.vercel.app">Feito por Alessandro ❤</a></footer>
+        <button onClick={handleKanye}>Fale comigo Kanye</button>
+    
+        <button onClick={newMusic}>Peça uma musica para Kanye</button>
+        
+        <footer> 
+          <a target="_blank" rel="noopener noreferrer" href="https://alessandrordgs.vercel.app">Feito por Alessandro ❤</a>
+        </footer>
 
      </ div>
        
-
- 
-    </>
   );
 }
 
